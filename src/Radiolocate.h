7217e2eb2cca2da282cd17597c67337d474e067f
Garrett Brown <garbearucla@gmail.com>
2011-05-10 15:00:55 -0700

    Completely remove ioctl calls in favor of netlink sockets

--- src/Radiolocate.h
+++ src/Radiolocate.h
@@ -1,5 +1,1 @@
 
-
-
-void SetScanParams(iwreq &iwr, const char interface[], void *pointer = NULL, __u16 length = 0, __u16 flags = 0);
-std::vector<NetworkAccessPoint> GetAccessPoints(const char interface[], int socket);
